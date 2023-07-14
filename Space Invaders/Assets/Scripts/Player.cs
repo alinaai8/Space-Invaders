1c6713004b9edcb7a58ff1e1fe84c21719124785
Alina Custură <68774898+alinaai8@users.noreply.github.com>
2023-07-14 10:09:05 +0300

    Player Movement.

---
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * this.speed * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
+    }
+}
